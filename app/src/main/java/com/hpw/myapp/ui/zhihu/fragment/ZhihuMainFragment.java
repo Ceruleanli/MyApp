dcb5087b74b764fd1b1cd672f93b91e51de07ceb
CeruleanLee <cerulean_lee@163.com>
2017-02-23 16:46:24 +0800

    0223.1

--- app/src/main/java/com/hpw/myapp/ui/zhihu/fragment/ZhihuMainFragment.java
+++ app/src/main/java/com/hpw/myapp/ui/zhihu/fragment/ZhihuMainFragment.java
@@ -41,6 +41,7 @@ public class ZhihuMainFragment extends CoreBaseFragment<ZhihuMainPresenter, Zhih
     TabLayout tabs;
     @BindView(R.id.viewpager)
     ViewPager viewpager;
+
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.fab)
